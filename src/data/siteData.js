9262e99edc1403a17e5e693caa1a448a074bd6a3
Dabi <134859804+dabi938@users.noreply.github.com>
2026-07-19 15:06:11 +0300

    Update siteData.js

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -13,9 +13,9 @@ const siteData = {
   titleEnglish: 'Finote Birhan Media',
 
   descriptionEnglish:
-    'kun Mana Kiristaanaa Ortodoksi Tawaahidoo Itiyoophiyaatti Magaalaa Shaggar Kutaa Magaalaa Buraayyuutti Liinkii fuula miidiyaa hawaasaa
- Mana Barnootaa dilbataa Finoota Birhaan Kawaala Tsedeniyaa Simmany Qulqulleettii Dubroo Maaryaamii ittiin argannuudha',
- 
+ `kun Mana Kiristaanaa Ortodoksi Tawaahidoo Itiyoophiyaatti Magaalaa Shaggar Kutaa Magaalaa Buraayyuutti Liinkii fuula miidiyaa hawaasaa
+ Mana Barnootaa dilbataa Finoota Birhaan Kawaala Tsedeniyaa Simmany Qulqulleettii Dubroo Maaryaamii ittiin argannuudha`,
+
     descriptionAmharic: `ይህ በኢ/ኦ/ተ/ቤ/ክር፣ በሸ/ከ/የቡ/ክ/ከተማ የፄዴንያ ስመኝ ቅድስት ድንግል ማርያም ገዳም ፍኖተ ብርሃን ሰንበት ትምህርት ቤት ይፋዊ የማኅበራዊ ሚዲያ ገጽ ሊንኮች ማግኛ ፔጅ ነው።`,
 
   // Each link becomes one row. `icon` must match a key exported
