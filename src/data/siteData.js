c1d2e75923bd0b14fb30c0cd177156ae0ac692c4
Dabi <134859804+dabi938@users.noreply.github.com>
2026-07-20 08:12:38 +0300

    Update siteData.js

--- src/data/siteData.js
+++ src/data/siteData.js
@@ -15,7 +15,7 @@ const siteData = {
   descriptionEnglish: `kun Mana Kiristaanaa Ortodoksi Tawaahidoo Itiyoophiyaatti Magaalaa Shaggar Kutaa Magaalaa Buraayyuutti Liinkii fuula miidiyaa hawaasaa
 Mana Barnootaa Dilbataa Finoota Birhaan Kawaala Tsedeniyaa Simmany Qulqulleettii Dubroo Maaryaamii ittiin argannuudha`,
 
-descriptionAmharic: `ይህ በኢ/ኦ/ተ/ቤ/ክር፣ በሸ/ከ/የቡ/ክ/ከተማ የፄዴንያ ስመኝ ቅድስት ድንግል ማርያም ገዳም ፍኖተ ብርሃን ሰንበት ትምህርት ቤት ይፋዊ የማኅበራዊ ሚዲያ ገጽ ሊንኮች ማግኛ ፔጅ ነው።`,
+descriptionAmharic: `ይህ በኢ/ኦ/ተ/ቤ/ክ ፣ በሸ/ከ/የቡ/ክ/ከተማ የፄዴንያ ስመኝ ቅድስት ድንግል ማርያም ገዳም ፍኖተ ብርሃን ሰንበት ትምህርት ቤት ይፋዊ የማኅበራዊ ሚዲያ ገጽ ሊንኮች ማግኛ ፔጅ ነው።`,
 
   // Each link becomes one row. `icon` must match a key exported
   // from src/components/icons.jsx
